4ea7f9053953b0f22b11f5afb650c82e55c5453a
INSMUND <iuliashilonosova@gmail.com>
2017-03-14 14:21:44 +0200

    Задание 2 пункт 4

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -4,9 +4,17 @@
 public class Point {
     double x;
     double y;
+  //  double x2;
+  //  double y2;
 
-    public Point(double x, double y) {
+
+    public Point(double x, double y ) {
         this.x = x;
         this.y = y;
+
+    }
+
+    public static double distance(Point p1,Point p2){
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
     }
 }
