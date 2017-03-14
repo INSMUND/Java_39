b5c8765741f169874de0181df7ea646c5c4864f8
INSMUND <iuliashilonosova@gmail.com>
2017-03-14 13:49:06 +0200

    Задание 2 пункт 3

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Pavel1 on 3/14/2017.
+ */
+public class Point {
+    double x;
+    double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
