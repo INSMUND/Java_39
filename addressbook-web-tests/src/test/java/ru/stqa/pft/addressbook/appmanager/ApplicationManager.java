b603faf0bf824912dc0ff9da542b4a80e00cd886
INSMUND <iuliashilonosova@gmail.com>
2017-03-27 12:30:28 +0300

    добавлена возможность запуска тестов в различных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,7 +2,11 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 
 import java.util.concurrent.TimeUnit;
@@ -11,17 +15,26 @@ import java.util.concurrent.TimeUnit;
  * Created by Pavel1 on 3/24/2017.
  */
 public class ApplicationManager {
-    private FirefoxDriver wd;
+    private WebDriver wd;
 
     private NavigationHalper navigationHalper;
     private GroupHelper groupHelper;
     private SessionHalper sessionHalper;
     private ContactHalper contactHalper;
+    private String browser;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
 
     public void init() {
-        wd = new FirefoxDriver();
+      
+        if (browser.equals(BrowserType.FIREFOX))wd = new FirefoxDriver();
+        else
+            if(browser.equals(BrowserType.CHROME))wd=new ChromeDriver(); else
+               if (browser.equals(BrowserType.IE)) wd=new InternetExplorerDriver();
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHalper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHalper.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
@@ -8,7 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 public class NavigationHalper extends HelperBase{
 
-    public NavigationHalper(FirefoxDriver wd) {
+    public NavigationHalper(WebDriver wd) {
        super(wd);
     }
 
